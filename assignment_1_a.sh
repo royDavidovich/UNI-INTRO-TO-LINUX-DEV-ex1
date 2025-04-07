71b86ae48a31088bd8b4cc0e6bc5359746d829d7
HomieskiO <146701063+HomieskiO@users.noreply.github.com>
2025-04-07 17:10:46 +0300

    Add files via upload

---
+++ assignment_1_a.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! "$1" ]; then
+  echo "Insert how many blocks to get as param for the script"
+  exit 1
+fi
+
+NUM_BLOCKS=$1
+CSV_FILE="./res.csv"
+
+echo "hash,height,total,time,relayed_by,prev_block" > "$CSV_FILE"
+
+current_hash=$(curl -s https://api.blockcypher.com/v1/btc/main | grep '"hash"' | grep -o -E "[0-9a-f]{64}")
+
+count=0
+
+for (( i=0; i<NUM_BLOCKS; i++ )); do
+
+  block_data=$(curl -s "https://api.blockcypher.com/v1/btc/main/blocks/$current_hash")
+
+  hash=$(echo "$block_data" | grep '"hash"' | grep -o -E "[0-9a-f]{64}")
+  height=$(echo "$block_data" | grep '"height"' | grep -o -E "\d+")
+  total=$(echo "$block_data" | grep '"total"' | grep -o -E "\d+")
+  time=$(echo "$block_data" | grep '"time"' | grep -o -E "[0-9:TZ-]{20}")
+  relayed_by=$(echo "$block_data" | grep '"relayed_by"' | grep -o -E "\d+[0-9\.:]+")
+  prev_block=$(echo "$block_data" | grep '"prev_block"' | grep -o -E "[0-9a-f]{64}")
+
+  echo "$hash,$height,$total,$time,$relayed_by,$prev_block" >> "$CSV_FILE"
+
+  current_hash=$prev_block
+done
+
+echo $CSV_FILE
