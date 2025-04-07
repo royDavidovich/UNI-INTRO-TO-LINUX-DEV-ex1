de5326983f8bcc0489542d313106d553722bb44e
Roy Davidovich LINUX <roydavidovich2000@gmail.com>
2025-04-07 21:00:03 +0300

    fixed " \d+" to "[0-9]"

--- assignment_1_a.sh
+++ assignment_1_a.sh
@@ -19,10 +19,10 @@ for (( i=0; i<NUM_BLOCKS; i++ )); do
   block_data=$(curl -s "https://api.blockcypher.com/v1/btc/main/blocks/$current_hash")
 
   hash=$(echo "$block_data" | grep '"hash"' | grep -o -E "[0-9a-f]{64}")
-  height=$(echo "$block_data" | grep '"height"' | grep -o -E "\d+")
-  total=$(echo "$block_data" | grep '"total"' | grep -o -E "\d+")
+  height=$(echo "$block_data" | grep '"height"' | grep -o -E "[0-9]+")
+  total=$(echo "$block_data" | grep '"total"' | grep -o -E "[0-9]+")
   time=$(echo "$block_data" | grep '"time"' | grep -o -E "[0-9:TZ-]{20}")
-  relayed_by=$(echo "$block_data" | grep '"relayed_by"' | grep -o -E "\d+[0-9\.:]+")
+  relayed_by=$(echo "$block_data" | grep '"relayed_by"' | grep -o -E "[0-9]+[0-9\.:]+")
   prev_block=$(echo "$block_data" | grep '"prev_block"' | grep -o -E "[0-9a-f]{64}")
 
   echo "$hash,$height,$total,$time,$relayed_by,$prev_block" >> "$CSV_FILE"
